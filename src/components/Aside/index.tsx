2fb5b2e015602ea9a17f66b7da8d30c5a77b95ec
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-02 01:57:49 +0100

    Update Aside to display new components

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,5 +1,9 @@
 import DevCard from '../inner/aside/DevCard'
 import Nav from '../inner/aside/Nav'
+import OpenSourceCard from '../inner/aside/OpenSourceCard'
+import Socials from '../inner/aside/Socials'
+import StickerCard from '../inner/aside/StickerCard'
+import Tags from '../inner/aside/Tags'
 import styles from './Aside.module.css'
 
 const Aside = () => {
@@ -7,6 +11,10 @@ const Aside = () => {
         <aside className={styles.container}>
             <DevCard />
             <Nav />
+            <Socials />
+            <Tags />
+            <StickerCard />
+            <OpenSourceCard />
         </aside>
     )
 }
