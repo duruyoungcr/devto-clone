b4d17318aaa70055a5118ade176b73425b9565f2
Duru-young Raymond <duruyoungcr@gmail.com>
2022-03-02 15:10:09 +0100

    Displayed the new aside components and styled them

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -1,8 +1,19 @@
+import Listings from '../inner/highlights/Listings'
+import TagListings from '../inner/highlights/TagListings'
+import TrendingListings from '../inner/highlights/TrendingListings'
 import styles from './Highlights.module.css'
+
+const tags = ['help', 'discuss', 'explainlikeimfive', 'challenge', 'meta', 'watercooler']
 
 const Highlights = () => {
     return (
-        <aside className={styles.container}>Highlights</aside>
+        <aside className={styles.container}>
+            <Listings />
+            {tags.map((tag, index) => (
+                <TagListings key={index} tag={tag} />
+            ))}
+            <TrendingListings />
+        </aside>
     )
 }
 
